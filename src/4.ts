fac033d5fec77c63001dcfd723616287af98118f
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2023-12-10 12:26:53 +0100

    done

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,78 @@
-const key = new Key();
+// У цьому завдання вам належить реалізувати сценарій життя, де людина, ключ і будинок взаємодіють один з одним.
 
+// 1. Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature, 
+// яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()). 
+// Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
+
+// 2. Людина (Person): Створіть клас Person. 
+// Конструктор цього класу приймає об'єкт класу Key і зберігає їх у приватному властивості key. 
+// Клас Person повинен мати метод getKey, який повертає збережений ключ.
+
+// 3. Дім (House): Створіть абстрактний клас House. 
+// Цей клас має дві властивості: door, яка може бути відкрита (true), 
+// або закрита (false), і key, яка зберігає об'єкт класу Key. 
+// У цьому класі також повинен бути метод comeIn, який додає об'єкт 
+// класу Person у масив tenants, якщо door відкрита. 
+// Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
+
+// 4. Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House. 
+// Реалізуйте метод openDoor у цьому класі. Якщо ключ, переданий цьому методу, 
+// збігається з ключем, збереженим як key, то двері відчиняються.
+
+// Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
+
+
+
+
+
+class Key {
+    private signature:number;
+    constructor(){
+        this.signature=Math.random()
+    }
+    getSignature():number{
+        return this.signature
+    }
+    
+}
+
+class Person {
+    constructor(private key:Key){
+        this.key = key;
+    }
+    getKey():Key{
+        return this.key;
+    }
+}
+
+abstract class House{
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(persone:Person):void{
+if(this.door){
+    this.tenants.push(persone)
+} else {
+    console.log("sorry door is close")
+}
+  }
+}
+
+class MyHouse extends House {
+    openDoor(key:Key){
+       if(key.getSignature()=== this.key.getSignature() ) {
+        this.door = true;
+       } else{
+        console.log("Sorry, key is wrong, try again later...")
+       }
+    }
+}
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
